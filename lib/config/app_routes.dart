2624129836a0921020fdb2831f77ff160c1a03c9
sejun2 <qpfjf56@gmail.com>
2022-12-24 03:08:07 +0900

    add detailpage route name

--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,6 +1,8 @@
+import 'package:comento_assignment/pages/detail/detail_page.dart';
 import 'package:comento_assignment/pages/list/list_page.dart';
 import 'package:flutter/material.dart';
 
 final appRoutes = <String, WidgetBuilder>{
   ListPage.routeName : (context) => const ListPage(),
+  DetailPage.routeName : (context)  => const DetailPage(),
 };
