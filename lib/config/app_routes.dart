2d15f9ce8a84c6c2a3bd6a5e8de36178a433aaf7
sejun2 <qpfjf56@gmail.com>
2022-12-24 02:58:19 +0900

    add routes

---
+++ lib/config/app_routes.dart
@@ -0,0 +1,6 @@
+import 'package:comento_assignment/pages/list/list_page.dart';
+import 'package:flutter/material.dart';
+
+final appRoutes = <String, WidgetBuilder>{
+  ListPage.routeName : (context) => const ListPage(),
+};
