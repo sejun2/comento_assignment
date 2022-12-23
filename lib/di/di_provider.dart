ba55f1a5160075cdcf4ad6ddd029feefc0876a34
sejun2 <qpfjf56@gmail.com>
2022-12-24 02:45:17 +0900

    feat di

--- lib/di/di_provider.dart
+++ lib/di/di_provider.dart
@@ -1,9 +1,11 @@
-import 'package:comento_assignment/repositories/comento_repository.dart';
-import 'package:comento_assignment/repositories/comento_repository_impl.dart';
+import 'package:comento_assignment/dio_provider.dart';
 import 'package:get_it/get_it.dart';
 
+import '../data/repositories/comento_repository.dart';
+import '../data/repositories/comento_repository_impl.dart';
+
 class DIProvider{
-  static provide(){
-    GetIt.I.registerSingleton<ComentoRepository>(ComentoRepositoryImpl(), instanceName: 'ComentoRepositoryImpl');
+  static void provide(){
+    GetIt.I.registerSingleton<ComentoRepository>(ComentoRepositoryImpl(comentoApiClient: DioProvider.comentoApiClient), instanceName: 'ComentoRepositoryImpl');
   }
 }
