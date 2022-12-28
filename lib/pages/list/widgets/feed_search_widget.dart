9b03f95a149421657b8926572d2e8ec9fc76d61b
sejun2 <qpfjf56@gmail.com>
2022-12-28 13:46:16 +0900

    fix feedsearchWidget hint

--- lib/pages/list/widgets/feed_search_widget.dart
+++ lib/pages/list/widgets/feed_search_widget.dart
@@ -28,6 +28,7 @@ class _FeedSearchWidgetState extends State<FeedSearchWidget> {
               maxLines: 1,
               autofocus: false,
               decoration: const InputDecoration(
+                hintText: '찾을 제목을 입력하세요',
                 icon: Icon(Icons.search),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.all(Radius.circular(6)),
